a29ac68098f5f6c7ca915f8abfda7273cafb7101
Ryan Stolliker <ryan.stolliker@gmail.com>
2026-02-12 04:39:53 -0800

    Generate auth system

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :chaoschat, :scopes,
+  user: [
+    default: true,
+    module: Chaoschat.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :id,
+    schema_table: :users,
+    test_data_fixture: Chaoschat.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :chaoschat,
   ecto_repos: [Chaoschat.Repo],
   generators: [timestamp_type: :utc_datetime]
